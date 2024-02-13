e2844a860038e68a99b2beb54d6b038c869bc6fa
Fatih Aytekin <fatih@aytekin.me>
2024-02-13 13:05:07 +0300

    Change desing welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { AppProvider } from '@/context/AppContext'
-import './styles/App.css'
 
 import Message from '@/components/Message'
 
 function App() {
     return (
         <AppProvider>
-            <Message />
+            <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-indigo-900 to-purple-900 text-white font-poppins">
+                <Message />
+            </div>
         </AppProvider>
     )
 }
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
 
 import { useAppContext } from '@/context/AppContext'
+import { useEffect, useState } from 'react'
 
 const Message: React.FC = () => {
     const { user } = useAppContext()
+    const [isVisible, setIsVisible] = useState<boolean>(true)
+
+    useEffect(() => {
+        setIsVisible(true)
+
+        const timeout = setTimeout(() => {
+            setIsVisible(false)
+        }, 5000)
+
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [user])
 
     if (!user) return null
 
     return (
-        <div>
-            <div>Hoşgeldin</div>
-
-            <h1>
-                {user.first_name} {user.last_name} 🎉
+        <div className={`flex flex-col items-center justify-center h-full ${isVisible ? '' : 'invisible'}`}>
+            <div className="text-3xl mb-6 animate-fade animate-delay-[500ms]">Hoşgeldin</div>
+            <h1 className="text-7xl font-bold mb-10 animate-jump-in animate-once">
+                {user.first_name} {user.last_name}
             </h1>
         </div>
     )
