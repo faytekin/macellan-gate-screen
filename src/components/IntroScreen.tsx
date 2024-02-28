0e7c1849fc6a585bce4f38871124cd7f439628a5
Fatih Aytekin <fatih@aytekin.me>
2024-02-28 11:44:02 +0300

    Typo

--- src/components/IntroScreen.tsx
+++ src/components/IntroScreen.tsx
@@ -6,21 +6,24 @@ import 'dayjs/locale/tr'
 dayjs.locale('tr')
 
 const IntroScreen = () => {
-    const [currentDate, setCurrentDate] = useState(dayjs())
+    const [currentTime, setCurrentTime] = useState(dayjs())
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentDate(dayjs())
+            setCurrentTime(dayjs())
         }, 1000)
 
         return () => clearInterval(interval)
     }, [])
 
+    const formattedDate = currentTime.format('DD MMMM YYYY')
+    const formattedTime = currentTime.format('HH:mm:ss')
+
     return (
         <div className="flex flex-col items-center justify-center h-full">
             <div className="text-center">
-                <div className="text-9xl font-poppins mb-10">{currentDate.format('DD MMMM YYYY')}</div>
-                <div className="clock-time font-thin font-poppins">{currentDate.format('HH:mm:ss')}</div>
+                <div className="text-9xl font-poppins mb-10">{formattedDate}</div>
+                <div className="clock-time font-thin font-poppins">{formattedTime}</div>
             </div>
         </div>
     )
